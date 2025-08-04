81cf4816be5376e63a6d5e6124ae51f46f2124c3
Menchukovskiy <menchukovskiy@gmail.com>
2025-08-04 10:55:45 +0300

    CORRECTION 12

--- js/arPage.js
+++ js/arPage.js
@@ -1,7 +1,67 @@
 select = e => document.querySelector(e)
 selectAll = e => document.querySelectorAll(e)
 
-const initHomeIndastry = () => {
+const stage = select('.stage')
+
+const arPorfolioSetting = {
+    type: 'loop',
+    item: 4,
+    padding: 40,
+    speed: 200,
+    interval: 3000,
+    autoplay: false,
+    gap: 10,
+    borderID: 'phoneBoxAr',
+    wrapID: 'sliderBoxAr'
+}
+
+
+
+const initPhoneBorder = () => {
+    const borderBox = document.querySelector('#' + arPorfolioSetting.borderID)
+    const slideWidth = document.querySelector('.slideBox').offsetWidth
+    borderBox.style.width = slideWidth + 2 * arPorfolioSetting.gap + 5 + 'px'
+    const borderBoxHeight = borderBox.offsetHeight
+    const sliderWrap = document.querySelector('#' + arPorfolioSetting.wrapID)
+    sliderWrap.style.height = borderBoxHeight * 0.88 + 'px'
+}
+
+const initArPortfolioVideo = () => {
+    document.querySelectorAll('.slideBox').forEach(element => {
+        if (element.classList.contains('is-active')) {
+            element.querySelector('.ar_portfolio_box_video video').play()
+        } else {
+            element.querySelector('.ar_portfolio_box_video video').pause();
+            element.querySelector('.ar_portfolio_box_video video').currentTime = 0;
+        }
+    })
+}
+
+const initArPortfoloi = () => {
+    const spladeArPorfolio = new Splide('.splide', {
+        type: arPorfolioSetting.type,
+        perPage: arPorfolioSetting.item,
+        padding: arPorfolioSetting.padding + 'px',
+        speed: arPorfolioSetting.speed,
+        pagination: false,
+        interval: arPorfolioSetting.interval,
+        autoplay: arPorfolioSetting.autoplay,
+        pauseOnHover: true,
+        gap: arPorfolioSetting.gap + 'px',
+        focus: 'center',
+        easing: 'ease',
+        updateOnMove: true
+    }).mount();
+
+    spladeArPorfolio.on('moved', () => {
+        initArPortfolioVideo()
+    })
+
+    initPhoneBorder()
+    initArPortfolioVideo()
+}
+
+const initIndastry = () => {
     const nextBtn = document.querySelector('#home_indastry .next');
     const prevBtn = document.querySelector('#home_indastry .prev');
     const carousel = document.querySelector('#home_indastry .carousel');
@@ -71,9 +131,11 @@ const initHomeIndastry = () => {
     }
 }
 
+
 const init = () => {
     gsap.set(stage, { autoAlpha: 1 })
     initIndastry()
+    initArPortfoloi()
 }
 
 
